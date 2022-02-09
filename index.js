af159994db05a21ffcf5b897700d2180ffe85fe4
chanchal16 <rajputchanchal17@gmail.com>
2022-02-09 11:43:11 +0530

    feat - add responsive sidebar functionality

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ const scrollDiv = ()=>{
     elmntToView.scrollIntoView({behavior: "smooth"})
 }
 btn && btn.addEventListener('click',scrollDiv);
+
+/*****sidebar*** */
+let sideNavigation = document.getElementById('sidebar');
+let hamburger = document.getElementById('sidenav');
+
+const toggleSideNav = ()=>{
+    console.log('click')
+    sideNavigation.classList.toggle('active');
+}
+
+hamburger && hamburger.addEventListener('click',toggleSideNav);

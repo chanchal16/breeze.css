d5c3bdc3ef85ffe937f060002f7defc3591630be
chanchal16 <rajputchanchal17@gmail.com>
2022-02-16 17:08:02 +0530

    Update modal.js

--- Components/Modal/modal.js
+++ Components/Modal/modal.js
@@ -1,17 +1,17 @@
 /////dialogs////////////
-let modal = document.getElementById("modal1");
-let alert = document.getElementById("btn1");
-let closeBtn = document.getElementById('closebtn');
-// to toggle class
-const toggleModal = () => {
-  modal.classList.toggle("show-modal");
-};
-const windowOnClick = (e) => {
-  if (e.target === modal) {
-    toggleModal();
-  } 
-};
-// add event listeners
-alert && alert.addEventListener("click", toggleModal);
-closeBtn && closeBtn.addEventListener("click",toggleModal);
-window && window.addEventListener("click", windowOnClick);
+  let modal = document.getElementById("modal1");
+  let alert = document.getElementById("btn1");
+  let closeBtn = document.getElementById('closebtn');
+  // to toggle class
+  const toggleModal = () => {
+    modal.classList.toggle("show-modal");
+  };
+  const windowOnClick = (e) => {
+    if (e.target === modal) {
+      toggleModal();
+    } 
+  };
+  // add event listeners
+  alert && alert.addEventListener("click", toggleModal);
+  closeBtn && closeBtn.addEventListener("click",toggleModal);
+  window && window.addEventListener("click", windowOnClick);

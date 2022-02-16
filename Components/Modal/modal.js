eb8a0914543a2351cce044a92a3a7c3739840964
chanchal16 <rajputchanchal17@gmail.com>
2022-02-16 17:00:40 +0530

    Update - removed console log

--- Components/Modal/modal.js
+++ Components/Modal/modal.js
@@ -2,18 +2,16 @@
 let modal = document.getElementById("modal1");
 let alert = document.getElementById("btn1");
 let closeBtn = document.getElementById('closebtn');
-
+// to toggle class
 const toggleModal = () => {
-  console.log("opened");
   modal.classList.toggle("show-modal");
 };
-
 const windowOnClick = (e) => {
   if (e.target === modal) {
     toggleModal();
   } 
 };
-
+// add event listeners
 alert && alert.addEventListener("click", toggleModal);
 closeBtn && closeBtn.addEventListener("click",toggleModal);
 window && window.addEventListener("click", windowOnClick);
